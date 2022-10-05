6d80cc976cac63112968626f8db397a0a8d089f2
Santhosh A <santhoshpdkt@hotmail.com>
2022-10-05 14:51:37 +0530

    Create Repo
    C# console app (.Net6.0) has reference to C++/CLI app

---
+++ CLRInteropLib/CLRInteropLib.h
@@ -0,0 +1,22 @@
+#pragma once
+
+using namespace System;
+
+namespace CLRInteropLib {
+	public ref class Class1
+	{
+        
+
+		// TODO: Add your methods for this class here.
+	public:
+        property int GetNum
+        {
+        public:
+            int get()
+            {
+                return 1;
+            }
+        
+        }
+	};
+}
---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using CLRInteropLib;
+
+namespace CSharpConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Class1 c1 = new Class1();
+            int num = c1.GetNum;
+        }
+    }
+}
